ec70db2dfcc7102ef5ecb8bf468b4bd4fce85889
Andrew Young <andrewjyoung2@gmail.com>
2025-06-14 12:19:45 -0700

    Wrapper for GPU Accumulate

--- src/common/math.cu
+++ src/common/math.cu
@@ -16,12 +16,72 @@ __global__ void VectorMultiply(float* C,
 }
 
 //------------------------------------------------------------------------------
+__global__ void Accumulate(float* d_result, float* d_A)
+{
+  *d_result = 10.0f;
+}
+
+//------------------------------------------------------------------------------
+__host__ void AccumulateDevice(float* d_result, float* d_A, const size_t len)
+{
+  if (len >= MAX_CUDA_THREADS) {
+    throw std::runtime_error("len must be less than MAX_CUDA_THREADS");
+  }
+
+  Accumulate<<<1, len>>>(d_result, d_A);
+}
+
+//------------------------------------------------------------------------------
+__host__ float AccumulateHost(float* A, const size_t len)
+{
+  if (len >= MAX_CUDA_THREADS) {
+    throw std::runtime_error("len must be less than MAX_CUDA_THREADS");
+  }
+
+  const size_t bytes = len * sizeof(float);
+
+  // Allocate device memory
+  float* d_A { nullptr };
+  if (cudaSuccess != cudaMalloc(&d_A, bytes)) {
+    throw std::runtime_error("cudaMalloc failed");
+  }
+  float* d_result { nullptr };
+  if (cudaSuccess != cudaMalloc(&d_result, sizeof(float))) {
+    throw std::runtime_error("cudaMalloc failed");
+  }
+
+  // Copy inputs to device
+  if (cudaSuccess != cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice)) {
+    throw std::runtime_error("Failed transfer from host to device");
+  }
+
+  AccumulateDevice(d_result, d_A, len);
+
+  // Copy result to host
+  float result { 0.7734f };
+  if (cudaSuccess != cudaMemcpy(&result,
+                                d_result,
+                                sizeof(float),
+                                cudaMemcpyDeviceToHost)) {
+    throw std::runtime_error("Failed transfer from device to host");
+  }
+
+  // Cleanup
+  cudaFree(d_A);
+  cudaFree(d_result);
+
+  return result;
+}
+
+//------------------------------------------------------------------------------
 __host__ void VectorMultiplyDevice(float* C,
                                    float* A,
                                    float* B,
                                    const size_t len)
 {
-  assert(len < MAX_CUDA_THREADS); // TODO: throw exception
+  if (len >= MAX_CUDA_THREADS) {
+    throw std::runtime_error("len must be less than MAX_CUDA_THREADS");
+  }
 
   VectorMultiply<<<1, len>>>(C, A, B);
 }
--- src/common/math.hpp
+++ src/common/math.hpp
@@ -2,6 +2,10 @@
 
 namespace math {
 
+__host__ void AccumulateDevice(float* d_result, float* A, const size_t len);
+
+__host__ float AccumulateHost(float* A, const size_t len);
+
 __host__ void VectorMultiplyDevice(float* C,
                                    float* A,
                                    float* B,
